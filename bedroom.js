5504be3cb51120900ebc81257ca411d9e6037d14
apai12148 <97473664+apai12148@users.noreply.github.com>
2023-06-29 12:03:40 -0400

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,30 @@
+status = ""
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+}
+
+function preload(){
+    img = loadImage('bedroom.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+}
